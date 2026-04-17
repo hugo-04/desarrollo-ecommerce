58fd353a61a83e75f18ffcd7aa44e36b2dddc30d
hugo <edseldvegavega@gmail.com>
2026-04-17 12:33:44 -0500

    fix(prisma): add IF EXISTS to index migrations to prevent db lock

--- prisma/migrations/20260324051131_add_indexes/migration.sql
+++ prisma/migrations/20260324051131_add_indexes/migration.sql
@@ -1,20 +1,20 @@
 -- DropIndex
-DROP INDEX "brands_name_trgm_idx";
+DROP INDEX IF EXISTS "brands_name_trgm_idx";
 
 -- DropIndex
-DROP INDEX "categories_name_trgm_idx";
+DROP INDEX IF EXISTS "categories_name_trgm_idx";
 
 -- DropIndex
-DROP INDEX "categories_slug_trgm_idx";
+DROP INDEX IF EXISTS "categories_slug_trgm_idx";
 
 -- DropIndex
-DROP INDEX "products_description_trgm_idx";
+DROP INDEX IF EXISTS "products_description_trgm_idx";
 
 -- DropIndex
-DROP INDEX "products_name_trgm_idx";
+DROP INDEX IF EXISTS "products_name_trgm_idx";
 
 -- DropIndex
-DROP INDEX "products_sku_trgm_idx";
+DROP INDEX IF EXISTS "products_sku_trgm_idx";
 
 -- CreateIndex
 CREATE INDEX "brands_showInCarousel_idx" ON "brands"("showInCarousel");
@@ -29,13 +29,13 @@ CREATE INDEX "products_bestSeller_idx" ON "products"("bestSeller");
 CREATE INDEX "products_featured_bestSeller_idx" ON "products"("featured", "bestSeller");
 
 -- RenameIndex
-ALTER INDEX "categories_count_desc_idx" RENAME TO "categories_count_idx";
+ALTER INDEX IF EXISTS "categories_count_desc_idx" RENAME TO "categories_count_idx";
 
 -- RenameIndex
-ALTER INDEX "products_brand_id_idx" RENAME TO "products_brandId_idx";
+ALTER INDEX IF EXISTS "products_brand_id_idx" RENAME TO "products_brandId_idx";
 
 -- RenameIndex
-ALTER INDEX "products_category_id_idx" RENAME TO "products_categoryId_idx";
+ALTER INDEX IF EXISTS "products_category_id_idx" RENAME TO "products_categoryId_idx";
 
 -- RenameIndex
-ALTER INDEX "products_rating_desc_idx" RENAME TO "products_rating_idx";
+ALTER INDEX IF EXISTS "products_rating_desc_idx" RENAME TO "products_rating_idx";
